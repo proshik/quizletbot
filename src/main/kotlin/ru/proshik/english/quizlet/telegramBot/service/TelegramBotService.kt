b4fb109a4b881aadf7ae6b618d187f7f9210c387
proshik <proxoraleksandrovich@gmail.com>
2018-09-17 10:01:43 +0300

    initialize changes to telegram bot keyboards

--- src/main/kotlin/ru/proshik/english/quizlet/telegramBot/service/TelegramBotService.kt
+++ src/main/kotlin/ru/proshik/english/quizlet/telegramBot/service/TelegramBotService.kt
@@ -43,8 +43,8 @@ class TelegramBotService(@Lazy private val bot: TelegramBot) {
 
     private fun commandMessage(update: Update): BotApiMethod<Message> {
         return when (update.message.text.split(" ")[0]) {
-            "/start" -> SendMessage().setChatId(update.message?.chatId).setText("Start message")
-            "/help" -> SendMessage().setChatId(update.message?.chatId).setText("Help message")
+            "/start" -> SendMessage().setChatId(update.message?.chatId).setText("This bot can help you get information about studied sets on quizlet.com")
+//            "/help" -> SendMessage().setChatId(update.message?.chatId).setText("Help message")
             "/inlinekeyboard" -> keyboard(update)
             "/replaykeyboard" -> sendCustomKeyboard(update)
             else -> throw RuntimeException("Unexpected command")
@@ -62,7 +62,7 @@ class TelegramBotService(@Lazy private val bot: TelegramBot) {
     private fun defaultMessage(update: Update): BotApiMethod<Message> {
         return SendMessage()
                 .setChatId(update.message?.chatId)
-                .setText("Default message")
+                .setText("To send text messages, please use the keyboard provided.")
     }
 
     private fun keyboard(update: Update): BotApiMethod<Message> {
@@ -90,30 +90,30 @@ class TelegramBotService(@Lazy private val bot: TelegramBot) {
     fun sendCustomKeyboard(update: Update): BotApiMethod<Message> {
         val message = SendMessage()
         message.chatId = update.message.chatId.toString()
-        message.text = "Custom message text"
+        message.text = "Select"
 
         // Create ReplyKeyboardMarkup object
         val keyboardMarkup = ReplyKeyboardMarkup()
+
         // Create the keyboard (list of keyboard rows)
-        val keyboard = ArrayList<KeyboardRow>()
-        // Create a keyboard row
-        var row = KeyboardRow()
+        val rows = ArrayList<KeyboardRow>()
+
         // Set each button, you can also use KeyboardButton objects if you need something else than text
-        row.add("Row 1 Button 1")
-        row.add("Row 1 Button 2")
-        row.add("Row 1 Button 3")
-        // Add the first row to the keyboard
-        keyboard.add(row)
-        // Create another keyboard row
-        row = KeyboardRow()
-        // Set each button for the second line
-        row.add("Row 2 Button 1")
-        row.add("Row 2 Button 2")
-        row.add("Row 2 Button 3")
-        // Add the second row to the keyboard
-        keyboard.add(row)
+        val statistics = KeyboardRow()
+        statistics.add("Statistics")
+
+        val notifications = KeyboardRow()
+        notifications.add("Notifications")
+
+        val account = KeyboardRow()
+        account.add("Account")
+
+        rows.add(statistics)
+        rows.add(notifications)
+        rows.add(account)
+
         // Set the keyboard to the markup
-        keyboardMarkup.keyboard = keyboard
+        keyboardMarkup.keyboard = rows
         // Add it to the message
         message.replyMarkup = keyboardMarkup
 
