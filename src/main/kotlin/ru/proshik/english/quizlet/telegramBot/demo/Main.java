84606de7aa800f769d7c22d852bd5b5dc5fa2cd8
proshik <proxoraleksandrovich@gmail.com>
2018-09-19 19:55:48 +0300

    play with inline keyboard

---
+++ src/main/kotlin/ru/proshik/english/quizlet/telegramBot/demo/Main.java
@@ -0,0 +1,24 @@
+package ru.proshik.english.quizlet.telegramBot.demo;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Initialize Api Context
+        ApiContextInitializer.init();
+
+        // Instantiate Telegram Bots API
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        // Register our bot
+        try {
+            botsApi.registerBot(new BotApi20());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
