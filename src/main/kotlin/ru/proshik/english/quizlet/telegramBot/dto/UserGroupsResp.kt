a9f905ef13854f8fb90b52def19c01a6224c6ba7
proshik <proxoraleksandrovich@gmail.com>
2018-09-24 17:25:28 +0300

    add notification table and update account table. And entities of course.

--- src/main/kotlin/ru/proshik/english/quizlet/telegramBot/dto/UserGroupsResp.kt
+++ src/main/kotlin/ru/proshik/english/quizlet/telegramBot/dto/UserGroupsResp.kt
@@ -6,6 +6,6 @@ data class UserGroupsResp(val id: Long,
                           val url: String,
                           val name: String,
                           @JsonProperty("created_date") val createdDate: Long,
-                          val school: GroupSchoolResp,
+                          val school: GroupSchoolResp?,
                           val sets: List<SetResp>,
                           val members: List<GroupMemberResp>)
--- src/main/kotlin/ru/proshik/english/quizlet/telegramBot/model/Account.java
+++ src/main/kotlin/ru/proshik/english/quizlet/telegramBot/model/Account.java
@@ -1,10 +1,20 @@
 package ru.proshik.english.quizlet.telegramBot.model;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.Parameter;
+import ru.proshik.english.quizlet.telegramBot.service.model.ModeType;
 
 import javax.persistence.*;
+import java.io.IOException;
 import java.time.ZonedDateTime;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
 
 import static javax.persistence.GenerationType.SEQUENCE;
 import static org.hibernate.id.enhanced.SequenceStyleGenerator.SEQUENCE_PARAM;
@@ -12,6 +22,22 @@ import static org.hibernate.id.enhanced.SequenceStyleGenerator.SEQUENCE_PARAM;
 @Entity
 @Table(name = "account")
 public class Account {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+
+    private static Map<ModeType, Boolean> DEFAULT_MODE_TYPES = new HashMap<>();
+
+    static {
+        OBJECT_MAPPER.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        DEFAULT_MODE_TYPES.put(ModeType.LEARN, true);
+        DEFAULT_MODE_TYPES.put(ModeType.FLASHCARDS, true);
+        DEFAULT_MODE_TYPES.put(ModeType.WRITE, true);
+        DEFAULT_MODE_TYPES.put(ModeType.SPELL, true);
+        DEFAULT_MODE_TYPES.put(ModeType.TEST, true);
+        DEFAULT_MODE_TYPES.put(ModeType.MATCH, true);
+        DEFAULT_MODE_TYPES.put(ModeType.GRAVITY, false);
+    }
 
     @Id
     @GeneratedValue(strategy = SEQUENCE, generator = "account_id_seq")
@@ -26,6 +52,12 @@ public class Account {
 
     private String accessToken;
 
+    @Column(name = "enabled_modes")
+    private String enabledModes;
+
+    @OneToMany(mappedBy = "account", cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    private Set<Notification> notifications = new HashSet<>();
+
     @OneToOne
     @JoinColumn(name = "user_id")
     private User user;
@@ -37,6 +69,7 @@ public class Account {
         this.createdDate = createdDate;
         this.login = login;
         this.accessToken = accessToken;
+        this.enabledModes = getDefaultModeTypes();
     }
 
     public Long getId() {
@@ -61,5 +94,46 @@ public class Account {
 
     public void setUser(User user) {
         this.user = user;
+    }
+
+    public String getEnabledModes() {
+        return enabledModes;
+    }
+
+    public void setEnabledModes(String enabledModes) {
+        this.enabledModes = enabledModes;
+    }
+
+    public Set<Notification> getNotifications() {
+        return notifications;
+    }
+
+    public void setNotifications(Set<Notification> notifications) {
+        this.notifications = notifications;
+    }
+
+    private static String getDefaultModeTypes() {
+        return safeWriteModeTypes(DEFAULT_MODE_TYPES);
+    }
+
+    public static String writeEnabledModeTypes(Map<ModeType, Boolean> enabledModeTypes) {
+        return safeWriteModeTypes(enabledModeTypes);
+    }
+
+    public static Map<ModeType, Boolean> readEnablesModeTypes(String modeTypes) {
+        try {
+            return OBJECT_MAPPER.readValue(modeTypes, new TypeReference<HashMap<ModeType, Boolean>>() {
+            });
+        } catch (IOException e) {
+            throw new RuntimeException("deserialize modeTypes to map");
+        }
+    }
+
+    private static String safeWriteModeTypes(Map<ModeType, Boolean> modeTypes) {
+        try {
+            return OBJECT_MAPPER.writerWithDefaultPrettyPrinter().writeValueAsString(modeTypes);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("unexpected behavior");
+        }
     }
 }
